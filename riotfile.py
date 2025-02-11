f674db02df5121bf5021a181b81da4f06c74da92
brettlangdon <me@brett.is>
2025-02-11 14:09:32 -0500

    verbose with capture

--- riotfile.py
+++ riotfile.py
@@ -2740,7 +2740,7 @@ venv = Venv(
         ),
         Venv(
             name="ci_visibility",
-            command="pytest --no-ddtrace -s {cmdargs} tests/ci_visibility",
+            command="pytest --no-ddtrace -v {cmdargs} tests/ci_visibility",
             pkgs={
                 "msgpack": latest,
                 "coverage": latest,
