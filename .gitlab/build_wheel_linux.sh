6b928e7ced362f9a23608e6fbda13b2feca5856e
brettlangdon <brett.langdon@datadoghq.com>
2025-05-06 20:29:56 -0400

    fix scripts?

--- .gitlab/build_wheel_linux.sh
+++ .gitlab/build_wheel_linux.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -ex -o pipefail
+
+project_dir="${CI_PROJECT_DIR:-.}"
+
 python_bin="${1:-python}"
-wheelhouse="${1:-wheelhouse/}"
+wheelhouse="${2:-wheelhouse/}"
 
 "${python_bin}" -m pip install -U -r .gitlab/linux-build.requirements.txt
-"${python_bin}" -m pip wheel "${CI_PROJECT_DIR}" --no-deps --wheel-dir "${wheelhouse}"
+"${python_bin}" -m pip wheel "${project_dir}" --no-deps --wheel-dir "${wheelhouse}"
 "${python_bin}" -m twine check --strict "${wheelhouse}/*"
 
