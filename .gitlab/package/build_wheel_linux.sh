4151419bcb543480438d492cfcbc09585bde348a
brettlangdon <brett.langdon@datadoghq.com>
2025-05-06 21:02:58 -0400

    refactor a bit

--- .gitlab/package/build_wheel_linux.sh
+++ .gitlab/package/build_wheel_linux.sh
@@ -6,7 +6,7 @@ project_dir="${CI_PROJECT_DIR:-.}"
 python_bin="${1:-python}"
 wheelhouse="${2:-wheelhouse/}"
 
-"${python_bin}" -m pip install -U -r .gitlab/linux-build.requirements.txt
+"${python_bin}" -m pip install -U -r ".gitlab/package/requirements-${PYTHON_TAG}.txt"
 "${python_bin}" -m pip wheel "${project_dir}" --no-deps --wheel-dir "${wheelhouse}"
 "${python_bin}" -m twine check --strict "${wheelhouse}/*"
 
