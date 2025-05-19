0b95b8f88a4cc8623790e4fd957454185a492159
brettlangdon <brett.langdon@datadoghq.com>
2025-05-19 21:02:06 +0200

    remove ddtrace_run scenario

--- benchmarks/startup/scenario.py
+++ benchmarks/startup/scenario.py
@@ -10,6 +10,9 @@ class Startup(bm.Scenario):
     import_ddtrace_auto: bool
     import_flask: bool
     import_django: bool
+    runtime_metrics_enabled: bool
+    appsec_enabled: bool
+    profiling_enabled: bool
     env: str
 
     def run(self):
@@ -18,11 +21,18 @@ class Startup(bm.Scenario):
             parsed_env: dict[str, str] = json.loads(self.env)
             env.update(parsed_env)
 
+        if self.runtime_metrics_enabled:
+            env["DD_RUNTIME_METRICS_ENABLED"] = "1"
+        if self.appsec_enabled:
+            env["DD_APPSEC_ENABLED"] = "1"
+
         commands: list[str] = []
         if self.import_ddtrace:
             commands.append("import ddtrace")
         if self.import_ddtrace_auto:
             commands.append("import ddtrace.auto")
+        if self.profiling_enabled:
+            commands.append("import ddtrace.profiling.auto")
 
         # Always do this last, we want to import/patch ddtrace first
         # DEV: We don't need to create/start an app, we just want to see the impact on import time
