7108ad6600675d3aa8d5d4ccfae49915800dcdc2
vianney <vianney.ruhlmann@datadoghq.com>
2025-07-08 17:05:52 +0200

    fix(config): enable NativeWriter by default

--- ddtrace/settings/_config.py
+++ ddtrace/settings/_config.py
@@ -478,10 +478,10 @@ class Config(object):
         self._trace_writer_connection_reuse = _get_config(
             "DD_TRACE_WRITER_REUSE_CONNECTIONS", DEFAULT_REUSE_CONNECTIONS, asbool
         )
-        self._trace_writer_log_err_payload = _get_config("_DD_TRACE_WRITER_LOG_ERROR_PAYLOADS", True, asbool)
+        self._trace_writer_log_err_payload = _get_config("_DD_TRACE_WRITER_LOG_ERROR_PAYLOADS", False, asbool)
 
         # Use the NativeWriter instead of the AgentWriter
-        self._trace_writer_native = _get_config("_DD_TRACE_WRITER_NATIVE", False, asbool)
+        self._trace_writer_native = _get_config("_DD_TRACE_WRITER_NATIVE", True, asbool)
 
         # TODO: Remove the configurations below. ddtrace.internal.agent.config should be used instead.
         self._trace_agent_url = _get_config("DD_TRACE_AGENT_URL")
